336531a9fb22f028ac40c0712c64a939339f2050
Sanket <sanketj@trentiums.com>
2024-02-27 14:22:26 +0530

    Made Full Responsive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,81 @@
 // HAMBURGERMENU
+const MenuDishes = {
+    salad: {
+        img: "images/salad.jpg",
+        quote: "Eat your greens to fit into your jeans.",
+        dishes: [
+            {
+                dish: "Caesar (Green beans, soy sauce, chicken tenderloins)",
+                price: "14$"
+            },
+            {
+                dish: "Beef (Fish sauce, cucumber, beef steak)",
+                price: "16$"
+            },
+            {
+                dish: "Greek (Feta cheese, lemon, cucumber, pepper)",
+                price: "12$"
+            },
+            {
+                dish: "King prawn (Glass noodles, Thai basil, jumbo prawns)",
+                price: "12$"
+            },
+            {
+                dish: "Ricotta with tarragon (Ricotta, garlic, English peas)",
+                price: "12$"
+            },
+            {
+                dish: "Salmon and potato (Can salmon, eggs, baking potatoes)",
+                price: "16$"
+            },
+            {
+                dish: "Mango, avocado & macadamia",
+                price: "19$"
+            },
+            {
+                dish: "Bocconcini, tomato and basil",
+                price: "16$"
+            },
+            
+        ]
+    },
+    salad: {
+        img: "images/salad.jpg",
+        quote: "Eat your greens to fit into your jeans.",
+        dishes: [
+            {
+                dish: "Caesar (Green beans, soy sauce, chicken tenderloins)",
+                price: "14$"
+            },
+            {
+                dish: "Beef (Fish sauce, cucumber, beef steak)",
+                price: "16$"
+            },
+            {
+                dish: "Greek (Feta cheese, lemon, cucumber, pepper)",
+                price: "12$"
+            },
+            {
+                dish: "King prawn (Glass noodles, Thai basil, jumbo prawns)",
+                price: "12$"
+            },
+            {
+                dish: "Ricotta with tarragon (Ricotta, garlic, English peas)",
+                price: "12$"
+            },
+            {
+                dish: "Salmon and potato (Can salmon, eggs, baking potatoes)",
+                price: "16$"
+            },
+            {
+                dish: "Mango, avocado & macadamia",
+                price: "19$"
+            },
+            {
+                dish: "Bocconcini, tomato and basil",
+                price: "16$"
+            },
+            
+        ]
+    }
+}
